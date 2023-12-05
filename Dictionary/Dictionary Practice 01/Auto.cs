068a2c4930e32821a4c81813a765c2e4a8d66415
JuanBeltrame <jbeltrame@frro.utn.edu.ar>
2023-12-05 13:12:27 -0300

    Update

---
+++ Dictionary/Dictionary Practice 01/Auto.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dictionary_Practice_01
+{
+    internal class Auto
+    {
+        private string patente;
+        private string marca;
+        public int modelo;
+
+
+        public Auto(string patente, string marca, int modelo)
+        {
+            this.patente = patente;
+            this.marca = marca;
+            this.modelo = modelo;
+        }
+
+
+        public string Mostrar()
+        {
+            StringBuilder sb = new StringBuilder();
+
+            sb.AppendLine($"Patente: {patente}");
+            sb.AppendLine($"Marca: {marca}");
+            sb.AppendLine($"Modelo: {modelo}");
+
+            return sb.ToString();
+        }
+
+
+    }
+
+
+}
--- Dictionary/Dictionary Practice 01/Program.cs
+++ Dictionary/Dictionary Practice 01/Program.cs
@@ -1,2 +1,100 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using Dictionary_Practice_01;
+
+Auto auto1 = new Auto("1234", "Fiat", 2005);
+Auto auto2 = new Auto("1234", "Renault", 1998);
+Auto auto3 = new Auto("1234", "Peugeot", 2002);
+
+
+//----------------------------------------------------------------------------------------------------
+// Ejemplo 01
+Dictionary<string, int> agenda = new Dictionary<string, int>();
+
+// La KEY no se puede repetir, El valor SI se puede repetir
+agenda.Add("Lautaro", 123456);
+agenda.Add("Mauricio", 123459);
+agenda.Add("Lucas", 2343459);
+
+// Como Recorrer
+Console.WriteLine();       
+foreach (KeyValuePair<string, int> kvp in agenda)
+{
+    Console.ForegroundColor = ConsoleColor.Green; Console.WriteLine(kvp);
+}
+
+// Ahora bien, si lo quiero mostrar por separado seria de la siguiente manera
+Console.WriteLine();
+foreach (KeyValuePair<string, int> kvp in agenda)
+{
+    Console.ForegroundColor = ConsoleColor.Red; Console.WriteLine($"Nombre: {kvp.Key} Numero: {kvp.Value}");
+}
+
+// Para recorrer solo las keys
+Console.WriteLine();
+foreach (var kvp in agenda.Keys)
+{
+    Console.ForegroundColor = ConsoleColor.Blue; Console.WriteLine(kvp);
+}
+
+// Para recorrer solo los values
+Console.WriteLine();
+foreach (var kvp in agenda.Values)
+{
+    Console.ForegroundColor = ConsoleColor.Yellow; Console.WriteLine(kvp);
+}
+
+
+Console.WriteLine();
+foreach (KeyValuePair<string, int> kvp in agenda)
+{
+    if (agenda.ContainsKey("Lucas"))
+        Console.WriteLine("Lucas esta agendado...");
+    if (agenda.ContainsValue(123456))
+        Console.WriteLine("Exito");
+}
+
+Console.WriteLine();
+if (agenda.TryGetValue("Lautaro", out int value))
+{
+    Console.WriteLine(value);
+}
+
+
+//----------------------------------------------------------------------------------------------------
+// Ejemplo 02
+Dictionary<string, string> diccionario = new Dictionary<string, string>();
+
+// La KEY no se puede repetir, El valor SI se puede repetir
+diccionario.Add("Taco", "Mexico");
+diccionario.Add("Burrito", "Mexico");
+diccionario.Add("Pizza", "Italia");
+
+Console.WriteLine();
+foreach (KeyValuePair<string, string> kvp in diccionario)
+{
+    Console.ForegroundColor = ConsoleColor.Magenta; Console.WriteLine($"Key: {kvp.Key} Value: {kvp.Value}");
+}
+
+Console.WriteLine();
+// Para eliminar, se hace por medio de la Key
+diccionario.Remove("Taco");
+Console.WriteLine("---------------");
+foreach(var kvp in diccionario)
+{
+    Console.ForegroundColor = ConsoleColor.Cyan; Console.WriteLine($"Key: {kvp.Key} Value: {kvp.Value}");
+}
+
+Console.WriteLine();
+Console.WriteLine("---------------");
+// Los diccinarios estan indexados
+diccionario["Burrito"] = "USA";
+foreach (var kvp in diccionario)
+{
+    Console.ForegroundColor = ConsoleColor.DarkCyan; Console.WriteLine($"Key: {kvp.Key} Value: {kvp.Value}");
+}
+
+
+// Metodos ContainsKey/ContainsValue
+Console.WriteLine(diccionario.ContainsKey("Pasta"));
+Console.WriteLine(diccionario.ContainsKey("Burrito"));
+Console.WriteLine(diccionario.ContainsValue("Italia"));
+Console.WriteLine(diccionario.ContainsValue("Argentina"));
