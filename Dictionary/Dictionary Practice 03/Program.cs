55b25ba99a0021fdde41872066014b01697f304c
JuanBeltrame <jbeltrame@frro.utn.edu.ar>
2023-12-07 15:32:58 -0300

    Update

---
+++ Dictionary/Dictionary Practice 03/Program.cs
@@ -0,0 +1,23 @@
+﻿Dictionary<string, int> agendaTelefonica = new Dictionary<string, int>();
+
+agendaTelefonica.Add("Marcelo", 4714277);
+agendaTelefonica.Add("Franky", 4713197);
+agendaTelefonica.Add("Lucas", 4711005);
+agendaTelefonica.Add("Juan", 4714277);
+agendaTelefonica.Add("Ayelen", 4714277);
+
+
+foreach (var kvp in agendaTelefonica)
+{
+    Console.ForegroundColor = ConsoleColor.Red;
+    Console.WriteLine($"Nombre: {kvp.Key}, Telefono: {kvp.Value}");
+}
+
+Console.WriteLine("Ingresar nombre");
+string inputName = Console.ReadLine()!;
+
+if (agendaTelefonica.TryGetValue(inputName, out int result))
+{
+    agendaTelefonica.Add(inputName, result);
+}
+
