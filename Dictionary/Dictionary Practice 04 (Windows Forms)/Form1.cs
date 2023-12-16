23ebb0ae00c19f1d27097d4b19f74c065f4cbbc3
Juan Beltrame <juan_beltrame@hotmail.com>
2023-12-16 16:42:38 -0300

    Update

---
+++ Dictionary/Dictionary Practice 04 (Windows Forms)/Form1.cs
@@ -0,0 +1,10 @@
+namespace Dictionary_Practice_04__Windows_Forms_
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+    }
+}
