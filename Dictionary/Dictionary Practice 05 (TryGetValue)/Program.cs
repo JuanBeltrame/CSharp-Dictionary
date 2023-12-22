a63be1a8e9eaa0d75f8acd62b1b7a165554cb7a9
JuanBeltrame <jbeltrame@frro.utn.edu.ar>
2023-12-22 15:28:11 -0300

    Update on Dictionaries

---
+++ Dictionary/Dictionary Practice 05 (TryGetValue)/Program.cs
@@ -0,0 +1,25 @@
+﻿//----------Programa Principal----------
+
+Dictionary<string, string> fruits = new Dictionary<string, string>()
+{
+    {"apple", "red"},
+    {"banana", "yellow"},
+    {"orange", "orange"},
+    {"grape", "purple"},
+    {"kiwi", "brown"},
+    {"pear", "green"},
+    {"strawberry", "red"},
+    {"watermelon", "green"},
+    {"pineaple", "yellow"},
+    {"mango", "orange"},
+};
+
+if (fruits.TryGetValue("banana", out string? bananaColor))
+{
+    Console.WriteLine("Found Value: " + bananaColor);
+}
+else
+{
+    Console.WriteLine("Value not found");
+}
+Console.ReadKey();
